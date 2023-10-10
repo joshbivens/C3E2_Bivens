b4f0d27e0876704f7c0b0647d593fc509aec18b0
joshbivens <thejoshbivens@gmail.com>
2023-10-10 17:47:57 -0600

    Add project files.

---
+++ ConstructorDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student();
+            myStudent.Major = "Computer Programming";
+
+            Console.WriteLine("My Major is {0}.", myStudent.Major);
+        }
+    }
+}
---
+++ ConstructorDemo/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo
+{
+    class Student
+    {
+
+        //-------------------------------------
+        //Constructors
+        //-------------------------------------
+
+
+        //-------------------------------------
+        //Properties
+        //-------------------------------------
+        public string Major { get; set; }
+
+        private int score1;
+
+        public int Score1
+        {
+            get { return score1; }
+            set { score1 = value; CalcAverage(); }
+        }
+        private int score2;
+
+        public int Score2
+        {
+            get { return score2; }
+            set { score2 = value; CalcAverage(); }
+        }
+
+        private int score3;
+
+        public int Score3
+        {
+            get { return score3; }
+            set { score3 = value; CalcAverage(); }
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StudentNumber { get; set; }
+        public double Average { get; private set; }
+
+        //-------------------------------------
+        //Instance Methods
+        //------------------------------------
+        public void CalcAverage()
+        {
+            Average = (score1 + score2 + score3) / 3.0;
+        }
+    }
+}
